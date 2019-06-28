412452fbba319d471d64bfda26c4a1f481dd7f4f
Hadrien Croubois <hc@iex.ec>
2019-06-28 16:02:56 +0200

    gas is sometimes low

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ var PriceOracle = artifacts.require("PriceOracle");
 
 module.exports = async function(deployer, network, accounts)
 {
-	await deployer.deploy(PriceOracle, "0x0000000000000000000000000000000000000000", { gas: 2000000 });
+	await deployer.deploy(PriceOracle, "0x0000000000000000000000000000000000000000", { gas: 2500000 });
 	PriceOracleInstance = await PriceOracle.deployed();
 	console.log("PriceOracle deployed at address: " + PriceOracleInstance.address);
 };
